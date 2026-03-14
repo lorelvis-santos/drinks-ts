91327e02bd718cda22a2bcc1bc2853ea09926f69
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-14 08:03:15 -0400

    feat: enhance Header component with dynamic background and add search form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { useMemo } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const { pathname } = useLocation();
+  const isHome = useMemo(() => pathname === "/", [pathname]);
+
   return (
-    <header className="bg-slate-800">
+    <header
+      className={
+        isHome
+          ? "bg-[url(/bg.jpg)] bg-center bg-cover h-screen"
+          : "bg-slate-800"
+      }
+    >
       <div className="mx-auto container px-5 py-8">
         <div className="flex justify-between items-center">
           <div>
@@ -31,6 +41,44 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400/80 my-32 p-10 rounded-lg shadow flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="ingredient"
+                className="text-white font-bold text-lg"
+              >
+                Nombre o ingredientes
+              </label>
+              <input
+                type="text"
+                name="ingredient"
+                id="ingredient"
+                className="p-3 bg-white w-full rounded-lg focus:outline-none"
+                placeholder="Ej. Vodka, Tequila, Café..."
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="ingredient"
+                className="text-white font-bold text-lg"
+              >
+                Categoría
+              </label>
+              <select
+                name="ingredient"
+                id="ingredient"
+                className="p-3 bg-white w-full rounded-lg focus:outline-none"
+              />
+            </div>
+            <input
+              type="submit"
+              value="Buscar recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-black mt-3 uppercase rounded-lg p-2"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
