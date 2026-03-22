d08835c51881c919b5f8fb99b35e530a656723cd
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-21 20:22:22 -0400

    feat: add favorites slice to manage user favorite recipes in state

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,14 @@
 import { create } from "zustand";
 import { createRecipeSlice, type RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import {
+  createFavoritesSlice,
+  type FavoritesSliceType,
+} from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a),
   })),
 );
