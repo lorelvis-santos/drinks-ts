7e06dec286139b23f3a23d9d51f76127857ccc9c
Lorelvis Santos <santoslorelvis@gmail.com>
2026-03-22 19:22:04 -0400

    feat: add Notification component and integrate notification state management

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Modal from "../components/Modal";
 import { useAppStore } from "../stores/useAppStore";
+import Notification from "../components/Notification";
 
 export default function Layout() {
   const loadFavorites = useAppStore((state) => state.loadFromStorage);
@@ -18,6 +19,7 @@ export default function Layout() {
         <Outlet />
       </main>
       <Modal />
+      <Notification />
     </>
   );
 }
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,14 +1,21 @@
 import { create } from "zustand";
-import { createRecipeSlice, type RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createRecipeSlice, type RecipesSliceType } from "./recipeSlice";
+import {
+  createNotificationSlice,
+  type NotificationSliceType,
+} from "./notificationSlice";
 import {
   createFavoritesSlice,
   type FavoritesSliceType,
 } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
+export const useAppStore = create<
+  RecipesSliceType & FavoritesSliceType & NotificationSliceType
+>()(
   devtools((...a) => ({
     ...createRecipeSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
   })),
 );
